d9d80e99e7d66a74934a1686e0e7add978a44e45
Rohit1972 <rohitdogra1972@gmail.com>
2025-05-01 19:18:02 +0530

    delted node modules

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ import projectRoutes from "./routes/projectRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 connect();
 
 const app = express();
@@ -16,12 +20,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 app.use("/users", userRoutes);
 app.use("/projects", projectRoutes);
 app.use("/ai", aiRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
 
 export default app;
